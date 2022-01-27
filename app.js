b59682911fba13a261ccf3a2eeca86143a049d08
Vmbugua <vmbugua@cytonn.com>
2022-01-27 08:54:58 +0300

    Configured webpack and tailwind

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
+require('./components');
+require('./assets/scss/app.scss');
 
 
 const app = new Vue({
--- components/App.vue
+++ components/App.vue
@@ -1,5 +1,9 @@
 <template>
-    
+    <div class="w-full flex justify-center align-center">
+        <div class="shadow-lg shadow-purple-200 p-4 w-full md:w-96 h-52 rounded bg-gradient-to-bl from-purple-300 to-blue-900 flex flex-col justify-center items-center mt-4 max-w-[24rem]">
+            <p class="text-gray-100 font-semibold">Vinn Njeru</p>
+        </div>
+    </div>
 </template>
 
 <script>
