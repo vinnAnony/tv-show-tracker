6b4218a1c003b8f7cae0f9901ebc950f3d2bfcb9
Vmbugua <vmbugua@cytonn.com>
2022-01-29 23:25:42 +0300

    Added router -added signup,login & header component

--- components/App.vue
+++ components/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <Header/>
     <div class="w-full flex justify-center align-center">
         <div class="shadow-lg shadow-purple-200 p-4 w-full md:w-96 h-52 rounded bg-gradient-to-bl from-purple-300 to-blue-900 flex flex-col justify-center items-center mt-4 max-w-[24rem]">
             <p class="text-gray-100 font-semibold">Vinn Njeru</p>
@@ -7,8 +8,10 @@
 </template>
 
 <script>
+    import Header from "./views/Header";
     export default {
-        name: "App"
+        name: "App",
+        components: {Header}
     }
 </script>
 
---
+++ components/views/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "SignUp",
+        data(){
+            return {
+                signUpData:{}
+            }
+        },
+        methods: {
+            register(){
+                axios.post("http://localhost:4000/api/auth/register",this.signUpData)
+                    .then(response => {
+                        this.$emit('switchView');
+                        this.signUpData = {};
+                        console.log(response.message);
+                    })
+                    .catch(error => {
+                        console.error("There was an error!", error);
+                    });
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
